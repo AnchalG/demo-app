a34de2cba1ee9454ffc793d77cf6d60db848ad70
MSDOMAIN1\agupt29 <anchal.gupta@morningstar.com>
2024-06-01 22:32:15 +0530

    change to make node js use port 8080

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// var app = express();
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
+
+// app.listen(8080, function (req, res) {
+//   res.send('Hello World from 8080!');
+//   console.log('Ready on port %d', server.address().port);
+// });
+
+const http = require('http');
+// specifying the desired port
+const port = 8080;
+const server = http.createServer((req, res) => {
+  // adding port information to the response
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(`Hello, GeeksforGeeks! Server is running on port ${port}`);
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
+
+// server.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
